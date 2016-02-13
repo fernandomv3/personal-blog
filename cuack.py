760e359a341324c6b690b4b910fe213e9e2954c5
fernandomv3 <fernandomv3@gmail.com>
2016-02-12 23:33:52 -0500

    ##

--- cuack.py
+++ cuack.py
@@ -6,7 +6,7 @@ import json
 import getopt
 import csv
 
-template_dir = ""
+template_dir = ''
 jinja_env = None
 dev = False
 
@@ -26,28 +26,28 @@ def write_page(output_file,text):
     f.write(text)
 
 def usage():
-  print("Usage: python3 cuack.py\n")
+  print('Usage: python3 cuack.py\n')
 
 def parse_args():
   global dev
   inputFile = ""
   try:
-    opts, args = getopt.getopt(sys.argv[1:],"f:h:d",["file=","help","dev"])
+    opts, args = getopt.getopt(sys.argv[1:],'f:h:d',['file=','help','dev'])
   except getopt.GetOptError as err:
     print(err,file=sys.stderr)
     usage()
     sys.exit(2)
   for opt , arg in opts:
-    if opt in ("-h","--help"):
+    if opt in ('-h','--help'):
       usage()
       sys.exit()
-    elif opt in ("-f","--file"):
+    elif opt in ('-f','--file'):
       inputFile = arg
-    elif opt in ("-d","--dev"):
+    elif opt in ('-d','--dev'):
       dev = True
-  if inputFile == "":
-    inputFile = "pages.json"
-  with open(inputFile,"r") as f:
+  if inputFile == '':
+    inputFile = 'pages.json'
+  with open(inputFile,'r') as f:
     conf = f.read()
   return json.loads(conf) 
 
@@ -57,7 +57,7 @@ def main(argv):
   global dev
   global_vars = {}
   json_conf = parse_args()
-  template_dir = os.path.join(os.path.dirname(__file__),json_conf["template_dir"])
+  template_dir = os.path.join(os.path.dirname(__file__),json_conf['template_dir'])
   jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape= True)
 
   pages = json_conf['pages']
@@ -67,10 +67,10 @@ def main(argv):
       for csv_conf in page['csv']:
         csv_data = load_csv_values(csv_conf['file_name'],csv_conf['csv_params'])
         page['values'][csv_conf['var_name']] = csv_data
-    if 'globals' in json_conf:
-      global_vars = json_conf["global_vars"] 
+    if 'global_vars' in json_conf:
+      global_vars = json_conf['global_vars'] 
     result= template.render(name=page['name'],dev= dev,meta= pages,global_vars= global_vars,**page['values'])
-    write_page(json_conf['output_dir']+ "/" + page['name'],result)
+    write_page(json_conf['output_dir']+ '/' + page['name'],result)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
   main(sys.argv)
